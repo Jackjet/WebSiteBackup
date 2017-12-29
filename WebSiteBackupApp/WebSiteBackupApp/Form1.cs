84ab58837fc6e0d85af6c0f3c1aaa4159157c33b
tbg2015 <851470150@qq.com>
2017-12-29 13:58:15 +0800

    init

---
+++ WebSiteBackupApp/WebSiteBackupApp/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebSiteBackupApp
+{
+    public partial class Form1 : Form
+    {
+        static string SourceDirectory = ConfigurationManager.AppSettings["SourceDirectory"];
+
+        static string TargetDirectory = ConfigurationManager.AppSettings["TargetDirectory"];
+
+        static int week = Convert.ToInt32(ConfigurationManager.AppSettings["week"]);
+
+        static long interval = Convert.ToInt32(ConfigurationManager.AppSettings["interval"]) * 1000;
+
+        static System.Timers.Timer timer = new System.Timers.Timer();
+
+        static string adds = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer.Elapsed += timer_Tick;
+            timer.Interval = interval;
+
+            timer.Start();
+        }
+
+        static void timer_Tick(object sender, EventArgs e)
+        {
+            DateTime datetime = DateTime.Now;
+            DayOfWeek _week = datetime.DayOfWeek;
+
+            adds = @"\" + datetime.ToString("yyyy-MM-dd");
+            if (_week == (DayOfWeek)week)
+            {
+                string dcs = TargetDirectory + adds;
+
+                if (!Directory.Exists(dcs))
+                {
+                    CopyDirectoryWorker cdw = new CopyDirectoryWorker();
+                    //拷贝文件夹                
+                    cdw.OnCopyFile += cdw_OnCopyFile;
+                    cdw.WorkOvered += cdw_WorkOvered;
+                    cdw.SourceDirectory = SourceDirectory;
+                    cdw.AimDirectory = dcs;
+
+                    System.Threading.ThreadPool.QueueUserWorkItem((o) =>
+                    {
+                        cdw.CopyFiles();
+                    });
+                }
+            }
+        }
+
+        static void cdw_WorkOvered()
+        {
+
+        }
+
+        static void cdw_OnCopyFile(long lngHad, long lngCount, string strShow)
+        {
+
+        }
+    }
+}
---
+++ WebSiteBackupApp/WebSiteBackupApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Windows.Forms;
+using System.Configuration;
+using System.IO;
+using System.Timers;
+
+namespace WebSiteBackupApp
+{
+    static class Program
+    {       
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            Form1 frm = new Form1() { Height = 0, Width = 0, ShowInTaskbar = false, Visible = false ,ShowIcon= false};
+            System.Windows.Forms.Application.Run(frm);       
+        }      
+    }
+}
